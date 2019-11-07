c01cd44920f74577b8698cfdaf5d7d2c98d23cf8
alex <alexander.korichev@gmail.com>
2019-11-08 02:51:31 +0300

    убрал мусор

--- include/interface/DrawingModule.hpp
+++ include/interface/DrawingModule.hpp
@@ -21,8 +21,6 @@ public:
     const int SPACE_BETWEEN = 38;
     const int MIN_X = 150;
     const double SPACE_LINES = 38;
-    const double TOP_LINE = 9;
-
     const double TIME_TO_PIXELS = 1000;
 
     void addStep(StepData *step);
@@ -39,7 +37,6 @@ private:
         double &fromY,
         double toX);
     void makeLines(int N, double &initial_y, std::vector<QGraphicsItemGroup *> &lineHolder, std::string const &name);
-    // void drawStep(StepData &prev, StepData &curr);
 };
 
 #endif // DRAWING_MODULE_HPP
--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
 #include "iostream"
 
 #include "./include/interface/Interface.hpp"
-
-const int NUM_BUFFERS = 3;
-const int NUM_DEVICES = 3;
-const int NUM_SOURCES = 10;
-const int NUM_REQUESTS_TO_BE_DONE = 10000;
-
 #include <QApplication>
 #include <QWidget>
 #include <QLabel>
